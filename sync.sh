dc3161af4de85494e43a80d96e15088e3594bfdc
yun <yunyunpp96@outlook.com>
2022-05-08 08:58:03 +0800

    make sync

---
+++ sync.sh
@@ -0,0 +1,20 @@
+tmp=$(mktemp -d)
+
+git clone https://github.com/ScoopInstaller/Main $tmp/main -b master --depth 1
+git clone https://github.com/kidonng/sushi $tmp/sushi -b master --depth 1
+
+cp $tmp/main/bucket/{v2ray}.json bucket/
+cp $tmp/sushi/bucket/{v2ray-domain-list-community,v2ray-geoip,v2ray-rules-dat}.json bucket/
+
+sed -E -i 's/github\.com\/(.+)\/releases\/download/download.fastgit.org\/\1\/releases\/download/' bucket/v2ray.json
+sed -E -i 's/sushi\//jade\//' bucket/*
+sed -E -i 's/"v2ray",/"jade\/v2ray",/' bucket/*
+
+# https://stackoverflow.com/a/25149786
+if [[ $(git status --porcelain --untracked-files=no) ]]; then
+  git add -u
+  git config user.name "github-actions[bot]"
+  git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git commit -m "Sync"
+  git push
+fi
