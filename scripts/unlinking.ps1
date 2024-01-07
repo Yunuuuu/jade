4ada7e5142e5754aa9dead2c6035cabee4d95e91
yun <yunyunpp96@outlook.com>
2024-01-07 12:03:56 +0800

    integrate remove_link into a single script: unlinking.ps1

---
+++ scripts/unlinking.ps1
@@ -0,0 +1,23 @@
+# & \"$bucketsdir\\jade\\scripts\\unlinking.ps1\"
+param([string]$source = $(throw "Parameter missing: -source source"))
+
+$source = $source.TrimEnd("/").TrimEnd("\\")
+
+# if we have had persist data in the store, just create link and go
+if (Test-Path $source) {
+    write-host \"Unlinking $source\"
+    $source = (Get-Item -Path $source -Force)
+    if ($source.LinkType) {
+        $source_path = $source.FullName
+        # directory (junction)
+        if ($source -is [System.IO.DirectoryInfo]) {
+            # remove read-only attribute on the link
+            attrib -R /L $source_path
+            # remove the junction
+            Remove-Item -Path $source_path -Recurse -Force -ErrorAction SilentlyContinue
+        } else {
+            # remove the hard link
+            Remove-Item -Path $source_path -Force -ErrorAction SilentlyContinue
+        }
+    }
+}
